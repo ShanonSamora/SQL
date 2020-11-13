aef8bcf5f5b8d0ffc61bb3b08690a410c69abffd
Shanon Samora <60020892+ShanonSamora@users.noreply.github.com>
2020-11-13 17:45:26 -0300

    Update TPLIBRO12A_ShanonSamora.sql

--- TPLIBRO12A_ShanonSamora.sql
+++ TPLIBRO12A_ShanonSamora.sql
@@ -1,59 +1,72 @@
-/* 
+/*
 NO HACER EL C
 
-Shanon Samora 1º2ºA BE
+Shanon Samora 1Âº2ÂºA BE
 ------------------------------------------------------------------------------------
-A – Básicas
+A â€“ BÃ¡sicas
 
-1. Indicar cuáles son los títulos y autores de los libros cuyo tipo sea ‘no’ y sus precios
-originales superen los $21. 
+1. Indicar cuÃ¡les son los tÃ­tulos y autores de los libros cuyo tipo sea â€˜noâ€™ y sus precios
+originales superen los $21.
+*/
 
 SELECT TITULO,AUTOR
 FROM LIBRO
 WHERE
 TIPO = 'no' AND PRECIO_ORI > 21
 
-2. ¿Cuáles son los números, precios originales y ediciones de los libros cuyo tipo sea ‘no’
+/*
+2. Â¿CuÃ¡les son los nÃºmeros, precios originales y ediciones de los libros cuyo tipo sea â€˜noâ€™
 o que sus precios originales superen $21 y las ediciones sean posteriores a 1985?
+*/
 
 SELECT NRO_LIBRO,PRECIO_ORI,EDICION
 FROM LIBRO
 WHERE
 TIPO = 'no' OR PRECIO_ORI > 21 AND EDICION > 1985
 
+/*
 3. Obtener la lista autores y ediciones de los libros cuyos nombres de autores comiencen
 con la letra L
+*/
 
 SELECT *
 FROM LIBRO
 WHERE AUTOR LIKE 'L%'
 
+/*
 4. Obtener la lista de autores y ediciones de los libros cuyos nombres de autores no
 comiencen con la letra G. 
+*/
 
 SELECT *
 FROM LIBRO
 WHERE AUTOR NOT LIKE 'G%'
 
+/*
 5. Obtener la lista de autores, precios originales y ediciones de los libros cuyos autores
-tengan la hilera "RR" en algún lugar del nombre
+tengan la hilera "RR" en algÃºn lugar del nombre
+*/
 
 SELECT * FROM LIBRO
 WHERE AUTOR LIKE '%RR%'
 
-6. Obtener la lista de títulos y ediciones de aquellos libros cuyos precios originales no
-estén comprendidos entre $12 y $25.
+/*
+6. Obtener la lista de tÃ­tulos y ediciones de aquellos libros cuyos precios originales no
+estÃ©n comprendidos entre $12 y $25.
+*/
 
 SELECT TITULO, EDICION, PRECIO_ORI FROM LIBRO
 WHERE PRECIO_ORI > 25 OR PRECIO_ORI < 12
 
-7. Obtener todos los tipos y ediciones DISTINTAS(en tipo y Edición) de los libros,
-ordenado por Edición y año de edición ascendente.
+/*
+7. Obtener todos los tipos y ediciones DISTINTAS(en tipo y EdiciÃ³n) de los libros,
+ordenado por EdiciÃ³n y aÃ±o de ediciÃ³n ascendente.
+*/
 
 SELECT TITULO, TIPO, EDICION FROM LIBRO
 ORDER BY EDICION
 
-8. Listar los números, ediciones, tipos, precios originales, precios actuales y diferencias
+8. Listar los nÃºmeros, ediciones, tipos, precios originales, precios actuales y diferencias
 de precios para todos los libros clasificado por tipo y por dicha diferencia en forma
 ascendente, en ese orden.
 
@@ -61,72 +74,72 @@ SELECT NRO_LIBRO, EDICION, TIPO, PRECIO_ORI, PRECIO_ACT, (PRECIO_ACT - PRECIO_OR
 FROM LIBRO
 ORDER BY (PRECIO_ACT - PRECIO_ORI) ASC
 
-9. Listar los números, ediciones, tipos, precios originales, precios actuales y diferencias
+9. Listar los nÃºmeros, ediciones, tipos, precios originales, precios actuales y diferencias
 de precios para todos los libros de estudio.
 
 SELECT NRO_LIBRO, EDICION, TIPO, PRECIO_ORI, PRECIO_ACT, (PRECIO_ACT - PRECIO_ORI) AS DIFF
 FROM LIBRO
 WHERE TIPO = 'es'
 
-10. ¿Cuáles son los números, precios originales y ediciones de los libros cuyas ediciones
-son posteriores a 1985 y además o bien el tipo es ‘no’ o el precio original supera $21?
+10. Â¿CuÃ¡les son los nÃºmeros, precios originales y ediciones de los libros cuyas ediciones
+son posteriores a 1985 y ademÃ¡s o bien el tipo es â€˜noâ€™ o el precio original supera $21?
 
 SELECT TITULO,NRO_LIBRO,PRECIO_ORI,EDICION,TIPO FROM LIBRO
 WHERE EDICION > 1985 AND (TIPO = 'no' OR PRECIO_ORI > 21)
 
-11. ¿Cuáles son los títulos y nombres de los autores de los libros cuyo tipo sea ‘no’ o
+11. Â¿CuÃ¡les son los tÃ­tulos y nombres de los autores de los libros cuyo tipo sea â€˜noâ€™ o
 aquellos cuyos precios de origen superan $21?
 
 SELECT TITULO,AUTOR,PRECIO_ORI FROM LIBRO
 WHERE TIPO = 'no' OR PRECIO_ORI > 21
 
-12. Obtener la lista de títulos y precios originales de los libros que se editaron en 1948,
+12. Obtener la lista de tÃ­tulos y precios originales de los libros que se editaron en 1948,
 1978 y 1985
 
 SELECT TITULO,PRECIO_ORI FROM LIBRO
 WHERE EDICION = 1948 OR EDICION = 1978 OR EDICION = 1985
 
-13. Obtener los títulos y ediciones de los libros cuyos precios originales estén dentro del
+13. Obtener los tÃ­tulos y ediciones de los libros cuyos precios originales estÃ©n dentro del
 rango de $12 a $25 inclusive
 
 SELECT TITULO,EDICION,PRECIO_ORI FROM LIBRO
 WHERE PRECIO_ORI BETWEEN 12 AND 25
 
-14. Obtener la lista de títulos, precios originales y ediciones de aquellos libros cuyos títulos
-tengan las letras "R" y "S" en algún lugar y en ese orden.
+14. Obtener la lista de tÃ­tulos, precios originales y ediciones de aquellos libros cuyos tÃ­tulos
+tengan las letras "R" y "S" en algÃºn lugar y en ese orden.
 
 SELECT TITULO,PRECIO_ORI,EDICION FROM LIBRO
 WHERE TITULO LIKE '%R%' AND TITULO LIKE '%S%'
 
-15. Obtener la lista de títulos, precios originales y ediciones de aquellos libros que tengan
-la letra "A" en la segunda posición del título.
+15. Obtener la lista de tÃ­tulos, precios originales y ediciones de aquellos libros que tengan
+la letra "A" en la segunda posiciÃ³n del tÃ­tulo.
 
 SELECT TITULO,PRECIO_ORI,EDICION FROM LIBRO
 WHERE TITULO LIKE '% A%'
 
-16. Ordenar en secuencia ascendente por año de edición los títulos de los libros cuyo tipo
-sea ‘no’. Listar también la edición.
+16. Ordenar en secuencia ascendente por aÃ±o de ediciÃ³n los tÃ­tulos de los libros cuyo tipo
+sea â€˜noâ€™. Listar tambiÃ©n la ediciÃ³n.
 
 SELECT TITULO,EDICION FROM LIBRO
 WHERE TIPO = 'no'
 ORDER BY EDICION ASC
 
-17. Listar los números, ediciones y tipos de libros cuyos precios originales superen los $20.
-Clasificar por edición en forma descendente y por número de libro ascendente en ese
+17. Listar los nÃºmeros, ediciones y tipos de libros cuyos precios originales superen los $20.
+Clasificar por ediciÃ³n en forma descendente y por nÃºmero de libro ascendente en ese
 orden.
 
 SELECT NRO_LIBRO,EDICION,TIPO,PRECIO_ORI FROM LIBRO
 WHERE PRECIO_ORI > 20
 ORDER BY EDICION DESC, NRO_LIBRO ASC
 
-18. Listar los números, ediciones, tipos, precios originales, precios actuales y diferencias
+18. Listar los nÃºmeros, ediciones, tipos, precios originales, precios actuales y diferencias
 de precios para todos los libros cuyas diferencias de precio sean superiores a $10.
 
 SELECT NRO_LIBRO,EDICION,TIPO,PRECIO_ORI, PRECIO_ACT, (PRECIO_ACT - PRECIO_ORI) AS DIFF
 FROM LIBRO
 WHERE (PRECIO_ACT - PRECIO_ORI) > 10
 
-19. Listar los números, ediciones, tipos, precios originales, precios actuales y diferencias
+19. Listar los nÃºmeros, ediciones, tipos, precios originales, precios actuales y diferencias
 de precios para todos los libros cuyas diferencias de precio sean superiores a $10,
 clasificado por dicha diferencia en forma descendente. 
 
@@ -136,10 +149,10 @@ WHERE (PRECIO_ACT - PRECIO_ORI) > 10
 ORDER BY (PRECIO_ACT - PRECIO_ORI) DESC
 
 ------------------------------------------------------------------------------------
-B - Funciones de Agregación, Having
+B - Funciones de AgregaciÃ³n, Having
 
-1. Obtener la suma y el promedio de los precios originales y el mínimo y el máximo de los
-precios actuales para todos los libros cuyo año de edición sea mayor a 1970
+1. Obtener la suma y el promedio de los precios originales y el mÃ­nimo y el mÃ¡ximo de los
+precios actuales para todos los libros cuyo aÃ±o de ediciÃ³n sea mayor a 1970
 
 SELECT
 sum(precio_ori) as suma_PreciosOri,
@@ -149,9 +162,9 @@ max(precio_act) as max_PrecioAct
 FROM LIBRO
 WHERE EDICION > 1970
 
-2. Obtener de la suma total de la suma de los precios originales más los actuales, el
-promedio de dicha suma y el mínimo y el máximo de las diferencias de precios para
-todos los libros cuyo año de edición sea superior a 1970.
+2. Obtener de la suma total de la suma de los precios originales mÃ¡s los actuales, el
+promedio de dicha suma y el mÃ­nimo y el mÃ¡ximo de las diferencias de precios para
+todos los libros cuyo aÃ±o de ediciÃ³n sea superior a 1970.
 
 SELECT
 sum(precio_ori+precio_act) as sumaTotal,
@@ -161,8 +174,8 @@ max(precio_ori+precio_act) as difMax
 FROM LIBRO
 WHERE EDICION > 1970
 
-3. Listar la cantidad de libros, los distintos tipos de libros, el mínimo y el máximo del
-precio original, pero sólo para aquellos libros cuyo precio original supere los $45. 
+3. Listar la cantidad de libros, los distintos tipos de libros, el mÃ­nimo y el mÃ¡ximo del
+precio original, pero sÃ³lo para aquellos libros cuyo precio original supere los $45. 
 
 SELECT tipo, COUNT(*) as cantidad,
 min(precio_ori) as precioMin,
@@ -173,7 +186,7 @@ GROUP BY TIPO
 HAVING COUNT(*) > 0
 
 4. Listar los tipos de libros, totales de precios originales, promedios de precios actuales,
-resumidos por tipo de libro y para los libros cuyas ediciones no sean de 1946, pero sólo
+resumidos por tipo de libro y para los libros cuyas ediciones no sean de 1946, pero sÃ³lo
 para aquellos tipos de libros cuya sumatoria de precios originales supere $40.
 
 SELECT TIPO,
@@ -224,7 +237,7 @@ FROM LIBRO
 WHERE EDICION BETWEEN 1946 AND 1975
 GROUP BY TIPO
 
-9. Listar el salario máximo agrupado por el tipo de trabajo.
+9. Listar el salario mÃ¡ximo agrupado por el tipo de trabajo.
 
 SELECT TRABAJO,
 MAX(SALARIO) as salarioMax
@@ -241,22 +254,22 @@ WHERE DIRECCION LIKE '%cap%'
 GROUP BY TRABAJO
 
 ------------------------------------------------------------------------------------
-D - Consultas en varias tablas – uso de join (inner join – left join – right join)
+D - Consultas en varias tablas â€“ uso de join (inner join â€“ left join â€“ right join)
 
-1. Listar el Titulo, fecha de Préstamo y la fecha de Devolución de los Libros prestados
+1. Listar el Titulo, fecha de PrÃ©stamo y la fecha de DevoluciÃ³n de los Libros prestados
 
 SELECT libro.TITULO, PRESTAMO.F_PREST, prestamo.F_DEVOL
 FROM LIBRO
 inner join PRESTAMO on PRESTAMO.NRO_LIBRO = libro.NRO_LIBRO
 
-2. Listar el Nro de Libro, Titulo, fecha de Préstamo y la fecha de Devolución de los Libros
+2. Listar el Nro de Libro, Titulo, fecha de PrÃ©stamo y la fecha de DevoluciÃ³n de los Libros
 prestados
 
 SELECT libro.NRO_LIBRO, libro.TITULO, prestamo.F_PREST, prestamo.F_DEVOL
 FROM LIBRO
 inner join PRESTAMO on PRESTAMO.NRO_LIBRO = libro.NRO_LIBRO
 
-3. Listar el número de lector, su nombre y la cantidad de préstamos realizados a ese
+3. Listar el nÃºmero de lector, su nombre y la cantidad de prÃ©stamos realizados a ese
 lector.
 
 SELECT LECTOR.NRO_LECTOR, lector.NOMBRE, COUNT(prestamo.F_PREST) as cantPrestamos
@@ -265,7 +278,7 @@ left join PRESTAMO on PRESTAMO.NRO_LECTOR = LECTOR.NRO_LECTOR
 GROUP BY LECTOR.NRO_LECTOR, lector.NOMBRE
 HAVING COUNT(prestamo.F_PREST) > -1
 
-4. Listar el número de libro, el título, el número de copia, y la cantidad de préstamos
+4. Listar el nÃºmero de libro, el tÃ­tulo, el nÃºmero de copia, y la cantidad de prÃ©stamos
 realizados para cada copia de cada libro.
 
 SELECT libro.NRO_LIBRO, libro.TITULO, copias.NRO_COPIA, COUNT(prestamo.F_PREST) as cantPrestamos
@@ -275,8 +288,8 @@ inner join PRESTAMO on PRESTAMO.NRO_LIBRO = LIBRO.NRO_LIBRO
 GROUP BY libro.NRO_LIBRO, libro.TITULO, copias.NRO_COPIA
 HAVING COUNT(prestamo.F_PREST) > -1
 
-5. Listar el número de libro, el título, y la cantidad de préstamos realizados para ese libro
-a partir del año 2012
+5. Listar el nÃºmero de libro, el tÃ­tulo, y la cantidad de prÃ©stamos realizados para ese libro
+a partir del aÃ±o 2012
 
 SELECT libro.NRO_LIBRO, libro.TITULO, COUNT(prestamo.F_PREST) as cantPrestamos
 FROM LIBRO
@@ -285,9 +298,9 @@ WHERE prestamo.F_PREST > 2012
 GROUP BY libro.NRO_LIBRO, libro.TITULO
 HAVING COUNT(prestamo.F_PREST) > -1
 
-6. Listar el número de libro, el título, el número de copia, y la cantidad de préstamos
-realizados para cada copia de cada libro, pero sólo para aquellas copias que se hayan
-prestado más de 1 vez.
+6. Listar el nÃºmero de libro, el tÃ­tulo, el nÃºmero de copia, y la cantidad de prÃ©stamos
+realizados para cada copia de cada libro, pero sÃ³lo para aquellas copias que se hayan
+prestado mÃ¡s de 1 vez.
 
 SELECT L.NRO_LIBRO, L.TITULO, P.NRO_COPIA, count(*) as cantidad
 FROM LIBRO L
@@ -295,7 +308,7 @@ INNER JOIN PRESTAMO P ON P.NRO_LIBRO = L.NRO_LIBRO
 GROUP BY L.NRO_LIBRO, L.TITULO, P.NRO_COPIA
 HAVING COUNT(*) > 1
 
-7. Listar el Nro de Libro, Titulo, nro de Copia y Fecha de Préstamo, de todas las Copias,
+7. Listar el Nro de Libro, Titulo, nro de Copia y Fecha de PrÃ©stamo, de todas las Copias,
 hayan sido prestadas o no.
 
 SELECT LIBRO.NRO_LIBRO, LIBRO.TITULO, COPIAS.NRO_COPIA, PRESTAMO.F_PREST
@@ -310,8 +323,8 @@ right join PRESTAMO on PRESTAMO.NRO_COPIA = COPIAS.NRO_COPIA
 right join LIBRO on LIBRO.NRO_LIBRO = COPIAS.NRO_LIBRO
 ORDER BY LIBRO.TITULO
 
-8. Listar Nro de Lector, Nombre, nro de Libro, Titulo, Descripción del Tipo de Libro , fecha
-de préstamo que aquellos Prestamos que hayan sido devueltos y el tipo de Libro sea
+8. Listar Nro de Lector, Nombre, nro de Libro, Titulo, DescripciÃ³n del Tipo de Libro , fecha
+de prÃ©stamo que aquellos Prestamos que hayan sido devueltos y el tipo de Libro sea
 Novela o Cuentos.
 
 SELECT LECTOR.NRO_LECTOR, LECTOR.NOMBRE, LIBRO.NRO_LIBRO, LIBRO.TITULO, PRESTAMO.F_PREST, PRESTAMO.F_DEVOL,
@@ -323,23 +336,23 @@ inner join TIPOLIBRO on TIPOLIBRO.TIPO = LIBRO.TIPO
 WHERE F_DEVOL IS NOT NULL AND ((TIPOLIBRO.DESCTIPO like 'Novela') OR (TIPOLIBRO.DESCTIPO like 'Cuentos'))
 ORDER BY TIPOLIBRO.DESCTIPO
 
-9. Obtener la lista de los títulos de los libros prestados y los nombres de los lectores que
-los tienen en préstamo
+9. Obtener la lista de los tÃ­tulos de los libros prestados y los nombres de los lectores que
+los tienen en prÃ©stamo
 
 SELECT LIBRO.TITULO, PRESTAMO.F_PREST, PRESTAMO.F_DEVOL, LECTOR.NOMBRE
 FROM PRESTAMO
 inner join LIBRO on PRESTAMO.NRO_LIBRO = LIBRO.NRO_LIBRO
 inner join LECTOR on LECTOR.NRO_LECTOR = PRESTAMO.NRO_LECTOR
 
-10. Listar el Nro de Lector, Nombre y fecha de Préstamo de aquellos Lectores que hayan
-realizado un préstamo y no lo hayan devuelto
+10. Listar el Nro de Lector, Nombre y fecha de PrÃ©stamo de aquellos Lectores que hayan
+realizado un prÃ©stamo y no lo hayan devuelto
 
 SELECT LECTOR.NRO_LECTOR, LECTOR.NOMBRE, PRESTAMO.F_PREST, PRESTAMO.F_DEVOL
 FROM LECTOR
 inner join PRESTAMO on PRESTAMO.NRO_LECTOR = LECTOR.NRO_LECTOR
 WHERE F_DEVOL IS NULL
 
-11. Listar Nro de Lector, Nombre, nro de Libro, Titulo, fecha de préstamo de aquellos
+11. Listar Nro de Lector, Nombre, nro de Libro, Titulo, fecha de prÃ©stamo de aquellos
 Prestamos que hayan sido devueltos.
 
 SELECT LECTOR.NRO_LECTOR, LECTOR.NOMBRE, LIBRO.NRO_LIBRO, LIBRO.TITULO, PRESTAMO.F_PREST, PRESTAMO.F_DEVOL
@@ -348,7 +361,7 @@ left join LECTOR on LECTOR.NRO_LECTOR = PRESTAMO.NRO_LECTOR
 left join LIBRO on LIBRO.NRO_LIBRO = PRESTAMO.NRO_LIBRO
 WHERE F_DEVOL IS NOT NULL
 
-12. Listar Nro de Lector, Nombre, nro de Libro, Titulo, fecha de préstamo de aquellos
+12. Listar Nro de Lector, Nombre, nro de Libro, Titulo, fecha de prÃ©stamo de aquellos
 Prestamos que hayan sido devueltos y el tipo de Libro sea Novela o Cuentos.
 
 SELECT LECTOR.NRO_LECTOR, LECTOR.NOMBRE, LIBRO.NRO_LIBRO, LIBRO.TITULO, PRESTAMO.F_PREST, PRESTAMO.F_DEVOL, LIBRO.TIPO
@@ -357,7 +370,7 @@ inner join LECTOR on LECTOR.NRO_LECTOR = PRESTAMO.NRO_LECTOR
 inner join LIBRO on LIBRO.NRO_LIBRO = PRESTAMO.NRO_LIBRO
 WHERE PRESTAMO.F_DEVOL IS NOT NULL AND ((LIBRO.TIPO LIKE 'no') OR (LIBRO.TIPO LIKE 'cu'))
 
-13. Listar el Nro de Lector, Nombre y fecha de Préstamo de Todos los Lectores, hayan
+13. Listar el Nro de Lector, Nombre y fecha de PrÃ©stamo de Todos los Lectores, hayan
 tenido Prestamos o no.
 
 SELECT LECTOR.NRO_LECTOR, LECTOR.NOMBRE, PRESTAMO.F_PREST
@@ -365,7 +378,7 @@ FROM LECTOR
 left join PRESTAMO on PRESTAMO.NRO_LECTOR = LECTOR.NRO_LECTOR
 
 ------------------------------------------------------------------------------------
-E – Insert – Update - Delete
+E â€“ Insert â€“ Update - Delete
 1. Agregar un nuevo LIBRO y sus respectivos valores a la tabla COPIAS.
 
 /*INSERT INTO LIBRO
@@ -380,7 +393,7 @@ UPDATE LIBRO
 SET PRECIO_ACT = '35.35'
 WHERE TITULO = 'LAS MIL Y UNA NOCHES'
 
-3. Eliminar los lectores cuyo salario sea menor $ 1000. ¿Cuántos lectores se han
+3. Eliminar los lectores cuyo salario sea menor $ 1000. Â¿CuÃ¡ntos lectores se han
 eliminado?
 
 DELETE FROM LECTOR
@@ -388,9 +401,9 @@ WHERE SALARIO < 1000
 -- SE ELIMINARON 4 LECTORES --
 
 ------------------------------------------------------------------------------------
-F – PLUS
+F â€“ PLUS
 1. Realizar una sentencia SQL que involucre TODOS los campos de TODAS las tablas
-¿Es esto posible?
+Â¿Es esto posible?
 
 SELECT *
 FROM LECTOR 
